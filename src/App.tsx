7de4e53a82af321b8a3e78a8a0fe59c6d3580c1c
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-19 20:05:11 +0330

    added weather queries

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ import CityPage from "./pages/CityPage"
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 min
+      gcTime: 10 * 60 * 1000, //10 min
+      retry: false,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 function App() {
   return (
