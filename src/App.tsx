605032e9c19fdff079b8df5f959132aeaf2038f9
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-19 16:56:27 +0330

    added dashboard and citypage initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
 import { ThemeProvider } from "./context/theme-provider"
+import Dashboard from "./pages/Dashboard"
+import CityPage from "./pages/CityPage"
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark">
         <Layout>
-          Hello
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
         </Layout>
       </ThemeProvider>
     </BrowserRouter>
