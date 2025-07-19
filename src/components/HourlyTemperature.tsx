dbd93ccf6f75722d8f67a9eb152cfdbcbfd3b318
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-20 01:19:22 +0330

    added tooltips for hourly chart

--- src/components/HourlyTemperature.tsx
+++ src/components/HourlyTemperature.tsx
@@ -1,6 +1,6 @@
 import type { ForecastData } from "@/api/types"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
-import {Line, LineChart, ResponsiveContainer, XAxis, YAxis} from 'recharts'
+import {Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts'
 import {format} from 'date-fns'
 
 interface HourlyTemperatureProps {
@@ -47,6 +47,28 @@ const HourlyTemperature = ({data}: HourlyTemperatureProps) => {
                             tickLine={false}
                             axisLine={false}
                             tickFormatter={(value) => `${value}°`}
+                        />
+
+                        <Tooltip
+                        content={({active, payload}) => {
+                            if (active && payload && payload.length) {
+                                return (
+                                    <div className="rounded-lg border bg-background p-2 shadow-sm">
+                                        <div className="grid grid-cols-2 gap-2">
+                                            <div className="flex flex-col">
+                                                <span className="text-[0.70rem] uppercase text-muted-foreground">Temperature</span>
+                                                <span className="font-bold"> {payload[0].value}°</span>
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <span className="text-[0.70rem] uppercase text-muted-foreground">Feels Like</span>
+                                                <span className="font-bold"> {payload[1].value}°</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                            return null
+                        }}
                         />
                         
                         <Line
