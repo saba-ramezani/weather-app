877f0b94cc99d7d473b84a4e79f562e2bdf7d0d9
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-19 19:32:34 +0330

    added geolocation hook and loading skeleton for dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,47 @@
+import LoadingSkeleton from "@/components/LoadingSkeleton"
+import { Button } from "@/components/ui/button"
+import { useGeolocation } from "@/hooks/use-geolocation"
+import { RefreshCcw } from "lucide-react"
 
 const Dashboard = () => {
+
+  const {coordinates,
+     error: locationError, 
+     getLocation, 
+     isLoading: locationLoading
+    } = useGeolocation()
+
+  console.log(coordinates)
+
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+      //reload weather data
+    }
+  }
+
+  if (locationLoading) {
+    return (
+      <LoadingSkeleton />
+    )
+  }
+
   return (
-    <div>Dashboard</div>
+    <div className="space-y-4">
+      {/* Favorite Cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button 
+          variant={'outline'}
+          size={'icon'}
+          onClick={handleRefresh}
+          // disabled={}
+          >
+          
+           <RefreshCcw className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
   )
 }
 
