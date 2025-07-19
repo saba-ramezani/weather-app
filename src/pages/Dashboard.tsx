5a3b1eed7d6f1e0b70ba011b0d900009dca04b3c
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-19 19:39:18 +0330

    added loading alert component for locationError in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,8 @@
 import LoadingSkeleton from "@/components/LoadingSkeleton"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
 import { useGeolocation } from "@/hooks/use-geolocation"
-import { RefreshCcw } from "lucide-react"
+import { AlertTriangle, MapPin, RefreshCcw } from "lucide-react"
 
 const Dashboard = () => {
 
@@ -22,7 +23,23 @@ const Dashboard = () => {
 
   if (locationLoading) {
     return (
-      <LoadingSkeleton />
+      <LoadingSkeleton /> 
+    )
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
     )
   }
 
