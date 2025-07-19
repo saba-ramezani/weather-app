4e60666aeea9d73f4a8dd19025169d705d12fa13
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-19 18:38:38 +0330

    added weatherAPI class and configs

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,27 @@ import Layout from "./components/Layout"
 import { ThemeProvider } from "./context/theme-provider"
 import Dashboard from "./pages/Dashboard"
 import CityPage from "./pages/CityPage"
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/city/:cityName" element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    
   )
 }
 
