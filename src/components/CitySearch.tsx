fb27d4604c8191af2ea436c20a089280b246a712
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-21 13:43:30 +0330

    added suggestions section for cities search dialog

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 import { Button } from "./ui/button"
-import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "./ui/command"
-import { Search } from "lucide-react";
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "./ui/command"
+import { Loader2, Search } from "lucide-react";
 import { useLocationSearch } from "@/hooks/use-weather";
 
 const CitySearch = () => {
     const [open, setOpen] = useState(false);
     const [query, setQuery] = useState("");
     const {data: locations, isLoading} = useLocationSearch(query)
+
+    const handleSelect = () => {
+
+    }
     console.log(locations)
   return (
     <>
@@ -27,11 +31,45 @@ const CitySearch = () => {
             />
             <CommandList>
                 {query.length > 2 && !isLoading && (<CommandEmpty>No cities found.</CommandEmpty>)}
-                <CommandGroup heading="Suggestions">
+                <CommandGroup heading="Favorites">
                     <CommandItem>Calendar</CommandItem>
-                    <CommandItem>Search Emoji</CommandItem>
-                    <CommandItem>Calculator</CommandItem>
                 </CommandGroup>
+                <CommandSeparator />
+                <CommandGroup heading="Recent Searches">
+                    <CommandItem>Calendar</CommandItem>
+                </CommandGroup>
+                <CommandSeparator />
+                {locations && locations.length > 0 && (
+                    <CommandGroup heading="Suggestions">
+                        {isLoading && (
+                            <div className="flex items-center justify-center p-4">
+                                <Loader2 className="h-4 w-4 animate-spin" />
+                            </div>
+                        )}
+                        {locations.map((location) => {
+                            return (
+                                <CommandItem
+                                key={`${location.lat}-${location.lon}`}
+                                value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                                onSelect={handleSelect}
+                                >
+                                    <Search className="mr-2 h-4 w-4" />
+                                    <span>{location.name}</span> 
+                                    {location.state && (
+                                        <span className="text-sm text-muted-foreground">
+                                            , {location.state}
+                                        </span>
+                                    )}
+                                    <span className="text-sm text-muted-foreground">
+                                        , {location.country}
+                                    </span>
+                                </CommandItem>
+                            )
+                        })}
+                        <CommandItem>Calendar</CommandItem>
+                    </CommandGroup>
+                )}
+                
             </CommandList>
         </CommandDialog>
     </>
