66d47bda1682684174862d6627389c2e7208b514
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-21 14:37:07 +0330

    added searchHistory to search dialog

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -1,18 +1,30 @@
 import { useState } from "react"
 import { Button } from "./ui/button"
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "./ui/command"
-import { Loader2, Search } from "lucide-react";
+import { Clock, Loader2, Search, XCircle } from "lucide-react";
 import { useLocationSearch } from "@/hooks/use-weather";
 import { useNavigate } from "react-router-dom";
+import { useSearchHistory } from "@/hooks/use-search-history";
+import {format} from 'date-fns'
 
 const CitySearch = () => {
     const [open, setOpen] = useState(false);
     const [query, setQuery] = useState("");
     const navigate = useNavigate();
     const {data: locations, isLoading} = useLocationSearch(query)
+    const {history, clearHistory, addToHistory} = useSearchHistory()
 
     const handleSelect = (cityData: string) => {
         const [lat, lon, name, country] = cityData.split("|")
+
+        addToHistory.mutate({
+            query,
+            name, 
+            lat: parseFloat(lat),
+            lon: parseFloat(lon),
+            country
+        })
+
         setOpen(false)
         navigate(`/city/${name}??lat=${lat}&lon=${lon}`)
     }
@@ -39,9 +51,49 @@ const CitySearch = () => {
                     <CommandItem>Calendar</CommandItem>
                 </CommandGroup>
                 <CommandSeparator />
-                <CommandGroup heading="Recent Searches">
-                    <CommandItem>Calendar</CommandItem>
-                </CommandGroup>
+                {history.length > 0 && (
+                    <>
+                        <CommandSeparator />
+                        <CommandGroup>
+                            <div className="flex items-center justify-between px-2 my-2">
+                                <p className="text-xs text-muted-foreground">
+                                Recent Searches
+                                </p>
+                                <Button
+                                variant="destructive"
+                                size="sm"
+                                className=" cursor-pointer text-sm"
+                                onClick={() => clearHistory.mutate()}
+                                >
+                                <XCircle className="h-3 w-3" />
+                                Clear
+                                </Button>
+                            </div>
+                            {history.map((item) => (
+                                <CommandItem
+                                className="cursor-pointer"
+                                key={item.id}
+                                value={`${item.lat}|${item.lon}|${item.name}|${item.country}`}
+                                onSelect={handleSelect}
+                                >
+                                    <Clock className="mr-2 h-4 w-4 text-muted-foreground" />
+                                    <span>{item.name}</span>
+                                    {item.state && (
+                                        <span className="text-sm text-muted-foreground">
+                                        , {item.state}
+                                        </span>
+                                    )}
+                                    <span className="text-sm text-muted-foreground">
+                                        , {item.country}
+                                    </span>
+                                    <span className="ml-auto text-xs text-muted-foreground">
+                                        {format(item.searchedAt, "MMM d, h:mm a")}
+                                    </span>
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </>
+                )}
                 <CommandSeparator />
                 {locations && locations.length > 0 && (
                     <CommandGroup heading="Suggestions">
